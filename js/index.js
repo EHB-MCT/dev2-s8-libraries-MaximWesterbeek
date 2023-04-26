eba787a2134757d276909b68119719feaa5345b3
MaximWesterbeek <114074573+MaximWesterbeek@users.noreply.github.com>
2023-04-26 15:49:57 +0200

    Ex 8 Done

--- js/index.js
+++ js/index.js
@@ -1,25 +1,38 @@
 "use strict";
 
 const app = {
-    map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
+    map: null,
     init() {
-        // initialise de kaart
+        this.map = L.map('map').setView([50.846662, 4.352541], 13);
 
-        // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-        // vergeet openstreetmap attributie niet
-        
-        // gebruik de functie "loadMarkers" om de markers toe te voegen
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.map);
+
+        app.loadMarkers();
+
     },
     loadMarkers() {
+        fetch('https://opendata.brussels.be/api/records/1.0/search/?dataset=toiletten&q=&rows=100&geofilter.distance=50.846475%2C+4.352793%2C+5000')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
 
-        // fetch de data van opendata.brussels.be
-            // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
-        
-        
+                data.records.forEach(function (toilets) {
+                    let lon = toilets.geometry.coordinates[0];
+                    let lat = toilets.geometry.coordinates[1];
+                    console.log(lat, lon);
+                    app.addMarker(lat, lon);
+                });
+            });
+
     },
     addMarker(lat, lon) {
-        // voeg een marker toe op lat, lon
+        this.marker = L.marker([lat, lon]).addTo(this.map)
+            .bindPopup('Lat: ' + lat + '<br> Lon: ' + lon)
+            .openPopup();
     }
-}
+};
 
 app.init();
